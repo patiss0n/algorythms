f5e9e870f42424d6d2c030c09a1e7bf4b9d2a444
Yurii <yurii.rudenko26@gmail.com>
2023-09-03 16:14:30 +0300

    added-Bubble-sort

---
+++ sortBubble.js
@@ -0,0 +1,18 @@
+function sortBubble(arr) {
+    let check = true
+    while(check == true) {
+        check = false
+        for(let i = 0; i < arr.length; i++) {
+            if(arr[i+1] < arr[i]) {
+                let temp = arr[i+1]
+                arr[i+1] = arr[i]
+                arr[i] = temp
+                check = true
+                console.log(arr)
+            }
+        }
+    }
+    return arr
+}
+
+console.log(sortBubble([100, 5, 3, 1, 9, -1, 0, 0, 12, 11, 18]))
